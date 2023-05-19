c49a52a3e61a0dfee05d6bc01e5e380829680331
nda000225 <nda000225@gmail.com>
2023-05-19 19:44:11 +0000

    send dynamic email using AWS SES with token

--- back/controllers/auth.js
+++ back/controllers/auth.js
@@ -1,31 +1,25 @@
 import * as config from "../config.js";
+import jwt from "jsonwebtoken";
+import { emailTemplate } from "../helpers/email.js";
 
 export const preRegister = async (req, res) => {
   //créer jwt avec e-mail et mot de passe puis e-mail en tant que lien cliquable
   //uniquement lorsque l'utilisateur clique sur ce lien e-mail, l'inscription est terminée
   try {
-    console.log(req.body);
+    const { email, passord } = req.body;
+    const token = jwt.sign({ email, passord }, config.JWT_SECRET, {
+      expiresIn: "1h",
+    });
     config.AWSSES.sendEmail(
-      {
-        Source: config.EMAIL_FROM,
-        Destination: {
-          ToAddresses: ["nda000225@gmail.com"],
-        },
-        Message: {
-          Body: {
-            Html: {
-              Charset: "UTF-8",
-              Data: `
-              <h1>Bienvenue à HOUSE AGENCY</h1>
-              `,
-            },
-          },
-          Subject: {
-            Charset: "UTF-8",
-            Data: "Bienvenue à HOUSE AGENCY",
-          },
-        },
-      },
+      emailTemplate(
+        email,
+        `
+        <p>Veuillez cliquer sur le lien ci-dessous pour activer votre compte.</p>
+        <a href="${config.CLIENT_URL}/auth/account-activate/${token}">Activation de mon compte</a>
+        `,
+        config.REPLY_TO,
+        "Activation de votre compte"
+      ),
       (err, data) => {
         if (err) {
           console.log(err);
@@ -36,6 +30,17 @@ export const preRegister = async (req, res) => {
         }
       }
     );
+  } catch (err) {
+    console.log(err);
+    return res.json({
+      error: "Quelque chose s'est mal passé. Essayer à nouveau.",
+    });
+  }
+};
+
+export const register = async (req, res) => {
+  try {
+    console.log(req.body);
   } catch (err) {
     console.log(err);
     return res.json({
--- back/routes/auth.js
+++ back/routes/auth.js
@@ -4,5 +4,6 @@ import * as auth from './../controllers/auth.js';
 const router = express.Router()
 
 router.post('/pre-register', auth.preRegister)
+router.post('/register', auth.register)
 
 export default router
