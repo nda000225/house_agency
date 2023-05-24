8fc0ce3a13267de3f88cd369d645e9edd786f993
nda000225 <nda000225@gmail.com>
2023-05-24 14:44:20 +0000

    refresh token using Axios interceptors in React and NodeJs

--- back/controllers/auth.js
+++ back/controllers/auth.js
@@ -8,7 +8,7 @@ import User from "../models/User.js";
 
 const tokenAndUserResponse = (res, user) => {
   const token = jwt.sign({ _id: user._id }, config.JWT_SECRET, {
-    expiresIn: "1d",
+    expiresIn: "10s",
   });
   const refreshToken = jwt.sign({ _id: user._id }, config.JWT_SECRET, {
     expiresIn: "7d",
